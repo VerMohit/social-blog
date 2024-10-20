7a54851b6e43528e134268b2f86a1c96b5c9d0fb
Mohit Verma <mohitver01@gmail.com>
2024-10-19 20:47:00 -0400

    added basic content for posts. Delete works, next need to add functionality for editing posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Text, Button, Stack } from "@mantine/core";
-import { ThemeProvider } from "./ThemeProvider";
+// import { Text, Button, Stack } from "@mantine/core";
+import { Container } from "@mantine/core";
+import Content from "./Content/Content";
+// import Layout from "./Layout/Layout";
 
 export default function App() {
+  const user: string = "admin";
+  const layoutProps = {
+    bg: "#dbe4ff",
+    h: "100vh",
+  };
+
   return (
-    <ThemeProvider>
-      <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
-      </Stack>
-    </ThemeProvider>
+    <Container {...layoutProps}>
+      <Content user={user} />
+    </Container>
   );
 }
